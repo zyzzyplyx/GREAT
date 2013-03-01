550ee515cbac2bf668e6a8b6df22c1e1ec0c00e3
Nicholas Damien McGee <ndmcgee@myth2.Stanford.EDU>
2013-02-28 19:35:49 -0800

    Changed bash script to run the python script

--- createRegDoms.sh
+++ createRegDoms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #From a list of transcription start sites, creates regions of +/- 1MB
-cat GREATRegDoms/ontologies/hg18/hg18.loci | awk 'BEGIN{} {print $2,$3-1000000,$3+1000000, $4,$5}' > hg18_regions.bed
+./createRegDoms.py GREATRegDoms/ontologies/hg18/hg18.loci hg18_regions.bed
 
 
 
