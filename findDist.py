ac39750a291b30b94a5a98dfb81f4a07cbe8640b
Nicholas Damien McGee <ndmcgee@corn05.stanford.edu>
2013-03-06 17:36:21 -0800

    Made associateTerms executable

--- findDist.py
+++ findDist.py
@@ -24,14 +24,14 @@ dist = scipy.stats.norm(0,regSz / 3)
 w_max = dist.pdf(0)
 
 for line in overlap:
-    line = line.split()
-    arrowPos = (int(line[1]) + int(line[2]))/2
-    lociCenter = int(line[10])
+  line = line.split()
+  arrowPos = (int(line[1]) + int(line[2]))/2
+  lociCenter = int(line[10])
 
-    distance = abs(lociCenter - arrowPos)
+  distance = abs(lociCenter - arrowPos)
 
-    outFile.write(line[3] + "\t" +  line[7] + "\t" + line[8] + "\t" +
-    str(dist.pdf(distance) / w_max) + "\n")
+  outFile.write(line[3] + "\t" +  line[7] + "\t" + line[8] + "\t" +
+  str(dist.pdf(distance) / w_max) + "\n")
 
 overlap.close()
 outFile.close()
